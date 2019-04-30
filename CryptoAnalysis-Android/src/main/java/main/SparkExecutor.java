edcd65b54df7fe25177feec1ec8a5754b57c28bd
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2019-04-30 17:45:27 +0200

    a parallel execution using Spark

--- CryptoAnalysis-Android/src/main/java/main/SparkExecutor.java
+++ CryptoAnalysis-Android/src/main/java/main/SparkExecutor.java
@@ -1,5 +1,12 @@
 package main;
 
+import fj.data.Java;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
@@ -26,21 +33,39 @@ public class SparkExecutor {
     }
 
     private static void startProcesses() {
-        List<File> listFiles = Arrays.stream(new File(appsDir).listFiles())
-                .filter(file -> file.getName().endsWith(".apk") || file.getName().endsWith(".APK"))
-                .collect(Collectors.toList());
+        try {
+            Logger.getLogger("org").setLevel(Level.OFF);
+            Logger.getLogger("akka").setLevel(Level.OFF);
 
-        listFiles.stream().forEach(apk -> {
-            try {
-                AnalysisResults res = SingleExecutor.run(apk.getAbsolutePath(), rulesDir, platformsDir);
-                System.out.println("Application: " + res.getApplication());
-                System.out.println("Analysis time: " + res.getTime());
-                System.out.println("Erros: " + res.getErrors().size());
-            }
-            catch(Exception e) {
-                System.err.println("Error processing " + apk.getName());
-                System.err.println("cause: " + e.getMessage());
-            }
-        });
+            //int cores = Runtime.getRuntime().availableProcessors();
+
+            List<File> listFiles = Arrays.stream(new File(appsDir).listFiles())
+                    .filter(file -> file.getName().endsWith(".apk") || file.getName().endsWith(".APK"))
+                    .collect(Collectors.toList());
+
+            //int tasksInParallel = listFiles.size() >= cores ? cores : listFiles.size();
+
+            SparkConf sparkConf = new SparkConf()
+                    .setAppName("Spark Executor for CogniCrypt")
+                    .setMaster("local");
+
+            JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+            ctx.setLogLevel("OFF");
+
+            JavaRDD<File> rddFiles = ctx.parallelize(listFiles);
+            JavaRDD<AnalysisResults> rddResults = rddFiles.map(f -> SingleExecutor.run(f.getAbsolutePath(), rulesDir, platformsDir));
+
+            Long time = rddResults.map(r -> r.getTime()).fold(new Long(0), (t1, t2) -> t1 + t2);
+            List<String> summary = rddResults.map(r -> r.getApplication() + ", " + r.getErrors().size() + ", " + r.getTime()).collect();
+
+            ctx.stop();
+
+            summary.forEach(s -> System.out.println(s));
+            System.out.println("Total analysis time " + time);
+        }
+        catch(Exception e) {
+            System.err.println("Error processing the apks");
+            System.err.println("cause: " + e.getMessage());
+        }
     }
 }
